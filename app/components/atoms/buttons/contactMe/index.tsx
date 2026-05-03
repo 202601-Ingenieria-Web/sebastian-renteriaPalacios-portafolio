17284f84fcefbb3d7a1911ae7814004dc2386d14
sren97 <sebastian.renteria@udea.edu.co>
2026-05-03 15:11:44 -0500

    Instalación de Iconify  + diseño de botón con ícono

---
+++ app/components/atoms/buttons/contactMe/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Icon } from "@iconify/react";
+const Index = () => {
+  return (
+    <div>
+        <div>
+            <button className="bg-secondary text-primary text-lg font-medium py-10px px-10px rounded-lg h-12.75 w-38.5 hover:cursor-pointer hover:bg-secondary-shadow hover:text-white transition duration-150 ease-in-out hover:-translate-y-1 hover:scale-110 flex items-center justify-center flex-row gap-2 ">
+            CONTACT ME
+            <Icon icon="material-symbols:arrow-forward-rounded" width="24" height="24" />
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Index
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-
+import ContactMe from "@/app/components/atoms/buttons/contactMe";
 
 export default function Home() {
   return (
     <div>
+    <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+    <div className="mt-4">
+      <ContactMe />
+    </div>
     </div>
   );
 }
